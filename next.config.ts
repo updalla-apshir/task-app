c3c60fba500df58bab4efcff56fb80f6501d6692
updalla-apshir <updallaapshir@gmail.com>
2025-06-20 14:37:10 +0300

    Fix prisma generation and build issues

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,20 @@
-// ✅ Correct next.config.js
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
+  },
+  // Flag to indicate that the app is being deployed on Vercel
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
